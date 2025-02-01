b95c90c51fe065a9c3ba82f99e32bdd6db55184a
Avijit Das <avijitavi338895@gmail.com>
2025-02-01 22:38:09 +0600

    Add images and fonts

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,9 +10,33 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          Stack(
+            children: [
+              Image.asset(
+                "assets/images/hill.jpg",
+                height: height / 3,
+                width: width,
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Image.asset(
+                    "assets/images/man.jpg",
+                    height: 40.0,
+                    width: 40.0,
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
