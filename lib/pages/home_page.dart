8ef4284d4ae2506b3bdab72b38550ce97f365664
Avijit Das <avijitavi338895@gmail.com>
2025-02-02 17:34:03 +0600

    Home UI update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -27,10 +27,64 @@ class _HomePageState extends State<HomePage> {
                 padding: const EdgeInsets.all(10.0),
                 child: Align(
                   alignment: Alignment.topRight,
-                  child: Image.asset(
-                    "assets/images/man.jpg",
-                    height: 40.0,
-                    width: 40.0,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20.0),
+                    child: Image.asset(
+                      "assets/images/man.jpg",
+                      height: 40.0,
+                      width: 40.0,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 60.0,
+                  left: 40.0,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Travelers",
+                      style: TextStyle(
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      "Travel community app",
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white54,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin:
+                    EdgeInsets.only(left: 30.0, right: 30.0, top: height / 3.4),
+                child: Material(
+                  elevation: 4.0,
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15.0),
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 1.5,
+                      ),
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                      ),
+                    ),
                   ),
                 ),
               )
