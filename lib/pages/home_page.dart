9f5b8583482088dc8e66d31610613ad05d61b09b
Avijit Das <avijitavi338895@gmail.com>
2025-02-05 22:43:55 +0600

    Project updated

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,84 +13,175 @@ class _HomePageState extends State<HomePage> {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Image.asset(
-                "assets/images/hill.jpg",
-                height: height / 3,
-                width: width,
-                fit: BoxFit.cover,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Align(
-                  alignment: Alignment.topRight,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20.0),
-                    child: Image.asset(
-                      "assets/images/man.jpg",
-                      height: 40.0,
-                      width: 40.0,
-                      fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Image.asset(
+                  "assets/images/hill.jpg",
+                  height: height / 3,
+                  width: width,
+                  fit: BoxFit.cover,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30.0),
+                      child: Image.asset(
+                        "assets/images/man.jpg",
+                        height: 40.0,
+                        width: 40.0,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 60.0,
-                  left: 40.0,
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 60.0,
+                    left: 40.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Travelers",
+                        style: TextStyle(
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "Travel community app",
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white54,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Travelers",
-                      style: TextStyle(
-                        fontSize: 50.0,
-                        fontWeight: FontWeight.bold,
+                Container(
+                  margin: EdgeInsets.only(
+                      left: 30.0, right: 30.0, top: height / 3.4),
+                  child: Material(
+                    elevation: 4.0,
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: Container(
+                      padding: EdgeInsets.only(left: 10.0),
+                      decoration: BoxDecoration(
                         color: Colors.white,
+                        borderRadius: BorderRadius.circular(15.0),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 1.5,
+                        ),
                       ),
-                    ),
-                    Text(
-                      "Travel community app",
-                      style: TextStyle(
-                        fontSize: 25.0,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white54,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                margin:
-                    EdgeInsets.only(left: 30.0, right: 30.0, top: height / 3.4),
-                child: Material(
-                  elevation: 4.0,
-                  borderRadius: BorderRadius.circular(15.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15.0),
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 1.5,
-                      ),
-                    ),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Search your destination",
+                          suffixIcon: Icon(
+                            Icons.search,
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              )
-            ],
-          )
-        ],
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Material(
+                elevation: 5.0,
+                borderRadius: BorderRadius.circular(
+                  10.0,
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(
+                      10.0,
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 10.0,
+                          left: 10.0,
+                        ),
+                        child: Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(30.0),
+                              child: Image.asset(
+                                "assets/images/man.jpg",
+                                height: 40.0,
+                                width: 40.0,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 8.0,
+                            ),
+                            Text(
+                              "Avijit Das",
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Image.asset(
+                        "assets/images/sajek.jpg",
+                        width: width,
+                        height: 250.0,
+                        fit: BoxFit.fill,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: 10.0,
+                          left: 10.0,
+                          right: 10.0,
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                              color: Colors.green,
+                            ),
+                            Text(
+                              "Sajek Valey, Rangamati",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
